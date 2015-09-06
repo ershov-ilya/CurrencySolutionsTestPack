7d308ea9c62b93ff26a527eadb4cd162e48b4956
ILYA ERSHOV <ershov.ilya@gmail.com>
2015-09-06 12:45:41 +0300

    root const define

--- remote_test/Bootstrap.php
+++ remote_test/Bootstrap.php
@@ -1,16 +1,24 @@
 <?php
 /**
- * @author <insert name>
- * @package test
- *
+ * Created by PhpStorm.
+ * User: ershov-ilya
+ * Website: ershov.pw
+ * GitHub : https://github.com/ershov-ilya
+ * Date: 06.09.2015
+ * Time: 13:25
  */
+
 class Bootstrap
 {
+    public static $output="Programm result:\n";
+
 	public static function main($argv)
 	{
-		// Empty
+        print_r($argv);
+        defined('PROGRAMM_ROOT_PATH') or define('PROGRAMM_ROOT_PATH', dirname(__FILE__));
+        print Bootstrap::$output;
+		exit(0);
 	}
 }
 
 Bootstrap::main($argv);
-
