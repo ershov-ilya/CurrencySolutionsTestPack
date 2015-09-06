6e2922777f7b0daafc7f158ad1240dd1dc1104a5
ILYA ERSHOV <ershov.ilya@gmail.com>
2015-09-06 13:00:57 +0300

    comments

--- remote_test/Bootstrap.php
+++ remote_test/Bootstrap.php
@@ -20,10 +20,13 @@ class Bootstrap
         try {
             if(DEBUG) print_r($argv);
             if(empty($argv[1])) throw new Exception('Error: no input file! Pass input file like: php ./Bootstrap.php statement.csv', 400);
+
+            // Read file
             defined('PROGRAMM_ROOT_PATH') or define('PROGRAMM_ROOT_PATH', dirname(__FILE__));
             $file=$argv[1];
             $content=file(PROGRAMM_ROOT_PATH.'/'.$file);
 
+            // Parse file
             foreach($content as $line=>$str){
                 $arr=explode(',', $str);
                 if(DEBUG) print_r($arr);
